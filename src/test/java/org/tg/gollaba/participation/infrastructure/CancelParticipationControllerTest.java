2daa0571c71869df5a1307bf32f44250cc7f825a
digital-hamster <waterlove1439@naver.com>
2023-10-18 16:52:05 +0900

    [feat] 투표 취소

---
+++ src/test/java/org/tg/gollaba/participation/infrastructure/CancelParticipationControllerTest.java
@@ -0,0 +1,41 @@
+package org.tg.gollaba.participation.infrastructure;
+
+import com.epages.restdocs.apispec.ResourceSnippetParametersBuilder;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.tg.gollaba.common.ControllerTestContext;
+
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static org.springframework.restdocs.payload.JsonFieldType.NULL;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.tg.gollaba.common.ApiDocumentUtils.fieldsWithBasic;
+
+class CancelParticipationControllerTest extends ControllerTestContext {
+
+    private static final String TAG = Tags.VOTING.tagName();
+    private static final String DESCRIPTION = Tags.VOTING.descriptionWith("투표 철회");
+
+    @Test
+    void success() {
+        given()
+            .when()
+            .delete("/v2/participation/{participantId}", 1L)
+            .then()
+            .log().all()
+            .apply(
+                document(
+                    identifier(),
+                    new ResourceSnippetParametersBuilder()
+                        .tag(TAG)
+                        .description(DESCRIPTION),
+                    responseFields(
+                        fieldsWithBasic(
+                            fieldWithPath("data").type(NULL).description("응답 데이터")
+                        )
+                    )
+                )
+            )
+            .status(HttpStatus.OK);
+    }
+}
