e92ba517c0a87f1356e3e12b6d30c87efc7d6ed7
digital-hamster <waterlove1439@naver.com>
2024-03-14 21:35:56 +0900

    [feat] 회원 정보(self) 조회 API 추가

--- src/main/java/org/tg/gollaba/user/controller/GetSelfController.java
+++ src/main/java/org/tg/gollaba/user/controller/GetSelfController.java
@@ -7,15 +7,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.tg.gollaba.auth.vo.AuthenticatedUser;
 import org.tg.gollaba.common.web.ApiResponse;
+import org.tg.gollaba.user.service.GetSelfService;
 
 @RestController
 @RequestMapping("/v2/users/me")
 @RequiredArgsConstructor
 public class GetSelfController {
+    private final GetSelfService service;
 
     @PreAuthorize("hasAnyAuthority('USER')")
     @GetMapping
-    ApiResponse<Void> getMe(AuthenticatedUser user) {
-        return ApiResponse.success();
+    ApiResponse<GetSelfService.UserDetails> getMe(AuthenticatedUser user) {
+        var userDetails = service.get(user.id());
+
+        return ApiResponse.success(
+            new GetSelfService.UserDetails(
+                userDetails.name(),
+                userDetails.email(),
+                userDetails.roleType(),
+                userDetails.providerType(),
+                userDetails.profileImageUrl(),
+                userDetails.backgroundImageUrl()
+            )
+        );
     }
 }
