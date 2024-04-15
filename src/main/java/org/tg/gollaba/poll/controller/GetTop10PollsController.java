640f26ebeba81de746d8c3c8e1e4bde60c506ff4
digital-hamster <waterlove1439@naver.com>
2024-04-15 23:29:56 +0900

    [fix] 1차 피드백 - service 남음

--- src/main/java/org/tg/gollaba/poll/controller/GetTop10PollsController.java
+++ src/main/java/org/tg/gollaba/poll/controller/GetTop10PollsController.java
@@ -1,7 +1,5 @@
 package org.tg.gollaba.poll.controller;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,11 +9,7 @@ import org.tg.gollaba.common.web.HashIdController;
 import org.tg.gollaba.common.web.HashIdHandler;
 import org.tg.gollaba.common.web.PageResponse;
 import org.tg.gollaba.poll.service.GetTop10PollsService;
-import org.tg.gollaba.poll.vo.PagePollSummary;
 import org.tg.gollaba.poll.vo.PollSummary;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/v2/polls/total-top10")
@@ -29,42 +23,9 @@ public class GetTop10PollsController extends HashIdController {
     }
 
     @GetMapping
-    public ApiResponse<PageResponse<PagePollSummary>> get(Pageable pageable){
-
-        var pollSummaries =  service.get(pageable);
-
-        var pagePollSummaries = convertPollId(pollSummaries);
-
+    public ApiResponse<PageResponse<PollSummary>> get(Pageable pageable){
         return ApiResponse.success(
-            new PageResponse<>(
-                pagePollSummaries.getContent(),
-                pagePollSummaries.getNumber(),
-                pagePollSummaries.getSize(),
-                pagePollSummaries.getTotalElements(),
-                pagePollSummaries.getTotalPages()
-            ));
-    }
-
-    private Page<PagePollSummary> convertPollId(Page<PollSummary> pollSummaries) {
-        List<PagePollSummary> hashedPollSummary = pollSummaries.getContent()
-            .stream()
-            .map(pollSummary -> new PagePollSummary(
-                createHashId(pollSummary.id()),
-                pollSummary.title(),
-                pollSummary.creatorName(),
-                pollSummary.responseType(),
-                pollSummary.pollType(),
-                pollSummary.endAt(),
-                pollSummary.readCount(),
-                pollSummary.totalVotingCount(),
-                PagePollSummary.PollItem.to(pollSummary.items())
-            ))
-            .collect(Collectors.toList());
-
-        return new PageImpl<>(
-            hashedPollSummary,
-            pollSummaries.getPageable(),
-            pollSummaries.getTotalElements()
+            PageResponse.from(service.get(pageable))
         );
     }
 }
